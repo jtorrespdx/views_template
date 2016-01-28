f206545c932ad6d4a534458a708b9796cfdf497b
jtorrespdx <jtorres.pdx@gmail.com>
2016-01-28 12:47:09 -0800

    add ability to show event type images -Kyle

--- views-view-fields--events-calendar--default.tpl.php
+++ views-view-fields--events-calendar--default.tpl.php
@@ -1,21 +1,27 @@
 <?php
   dpm($fields);
 
+  $type = str_replace(" ", "-", $fields['field_npf_event_type']->content);
+  $type = strtolower(str_replace(array("<span>","</span>"),"",$type));
 ?>
+<div class="event-container">
 
-<div>
-  <?php print $fields['title']->content; ?>
-  <?php print $fields['field_npf_event_date']->content; ?>
-</div>
-<div>
-  <?php print $fields['field_npf_event_venue_name']->content; ?>
-</div>
-<div>
-  <?php print $fields['field_npf_event_venue_address']->content; ?>
-</div>
-<div>
-  <?php print $fields['field_npf_event_description']->content; ?>
-</div>
-<div>
-  <?php print $fields['field_npf_event_venue_map']->content; ?>
+
+  <div>
+    <img src="/sites/default/files/calendar/<?php print $type ?>.jpg" alt="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_type']->content); ?>"></div>
+
+  </div>
+  <div>
+  <h2><strong><?php print $fields['title']->content; ?></strong></h2>
+
+    <h3><?php print $fields['field_npf_event_date']->content; ?><h3>
+
+    <p><strong><?php print $fields['field_npf_event_venue_name']->content; ?></strong></p>
+    <p><?php print $fields['field_npf_event_venue_address']->content; ?></p>
+
+    <p><?php print $fields['field_npf_event_description']->content; ?></p>
+  </div>
+  <div>
+    <?php print $fields['field_npf_event_venue_map']->content; ?>
+  </div>
 </div>
