a001cef81b5c7d2fcbaf9cb67f5acd9d9d790eb2
jtorrespdx <jtorres.pdx@gmail.com>
2016-01-29 14:32:17 -0800

    tweaks html structure.

--- views-view-fields--events-calendar--default.tpl.php
+++ views-view-fields--events-calendar--default.tpl.php
@@ -1,17 +1,16 @@
 <?php
-  dpm($fields);
+  // dpm($fields);
 
   $type = str_replace(" ", "-", $fields['field_npf_event_type']->content);
   $type = strtolower(str_replace(array("<span>","</span>"),"",$type));
 ?>
-<div class="event-container">
+<a target = "blank" href ="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_url']->content); ?>"><div class="event-container">
 
-
-  <div>
-    <img src="/sites/default/files/calendar/<?php print $type ?>.jpg" alt="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_type']->content); ?>"></div>
+  <div class="event-left">
+    <img src="/sites/default/files/calendar/<?php print $type ?>.jpg" alt="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_type']->content); ?>">
 
   </div>
-  <div>
+  <div class="event-middle">
   <h2><strong><?php print $fields['title']->content; ?></strong></h2>
 
     <h3><?php print $fields['field_npf_event_date']->content; ?><h3>
@@ -21,7 +20,7 @@
 
     <p><?php print $fields['field_npf_event_description']->content; ?></p>
   </div>
-  <div>
+  <div class="event-right">
     <?php print $fields['field_npf_event_venue_map']->content; ?>
   </div>
-</div>
+</div></a>
