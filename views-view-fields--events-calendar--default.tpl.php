183ac8b9e7ff56b100a3df4a314464800e36c3a7
jtorrespdx <jtorres.pdx@gmail.com>
2016-02-04 16:01:43 -0800

    modifies filter text

--- views-view-fields--events-calendar--default.tpl.php
+++ views-view-fields--events-calendar--default.tpl.php
@@ -7,25 +7,29 @@
 <a target = "blank" href ="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_url']->content); ?>">
   <div class="event-container">
 
-  <div class="event-left">
-    <img src="/sites/default/files/calendar/<?php print $type ?>.jpg" alt="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_type']->content); ?>">
+    <div class="event-left">
+      <img src="/sites/default/files/calendar/<?php print $type ?>.jpg" alt="<?php print str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_type']->content); ?>">
 
-  </div>
-  <div class="event-middle">
-  <h2><strong><?php print $fields['title']->content; ?></strong></h2>
-    <h3><?php print $fields['field_npf_event_date']->content; ?><h3>
+    </div>
+    <div class="event-middle">
+    <h2><strong><?php print $fields['title']->content; ?></strong></h2>
+      <h3><?php print $fields['field_npf_event_date']->content; ?><h3>
 
-    <address><strong><?php print $fields['field_npf_event_venue_name']->content; ?></strong>
-    <?php print $fields['field_npf_event_venue_address']->content; ?></address>
+      <address><strong><?php print $fields['field_npf_event_venue_name']->content; ?></strong>
+      <?php print $fields['field_npf_event_venue_address']->content; ?></address>
 
-    <p><?php print $fields['field_npf_event_description']->content; ?></p>
-  </div>
-  <div class="event-right">
-    <?php print $fields['field_event_location_map']->content; ?>
-  </div>
+      <p><?php print $fields['field_npf_event_description']->content; ?></p>
+    </div>
+    <div class="event-right">
+      <?php print $fields['field_event_location_map']->content; ?>
+    </div>
 </div>
 </a>
 
 <script>
+  // This just inserts "miles from" into the distance filter display.
+  document.getElementsByClassName("geofield-proximity-origin-from")[0].textContent = 'miles from';
+
+  //Insert placeholder attribute and "zip" text into distance origin field
   document.getElementById("edit-field-geofield-distance-origin").setAttribute("placeholder","Zip");
 </script>
