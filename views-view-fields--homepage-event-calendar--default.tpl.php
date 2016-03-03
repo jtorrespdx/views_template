55111f8c13d86090dda955aab4157e0be8b6675b
Kyle Pratuch <kyle.pratuch@gmail.com>
2016-03-03 12:02:59 -0800

    fix templating on homepage view

--- views-view-fields--homepage-event-calendar--default.tpl.php
+++ views-view-fields--homepage-event-calendar--default.tpl.php
@@ -1,8 +1,11 @@
 <?php
-  dpm($fields);
+  // dpm($fields);
 
   $url = str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_url']->content);
   $title = $fields['title']->content;
+  //Short title is truncated at x characters to fit in desktop layout.
+  // $short_title = mb_strimwidth($title, 0, 24, "...")."</span>";
+  //Longer title is truncated at x characters to fit in mobile layout.
   $state = str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_venue_address_administrative_area']->content);
   $city = str_replace(array("<div>", "</div>"),"", $fields['field_npf_event_venue_address_locality']->content);
   $date = $fields['field_npf_event_date']->content;
@@ -12,9 +15,10 @@
 <!-- Minipanel content pane layout -->
 <div class="homepage-calendar-feed-container">
   <a target="blank" href="<?php print $url; ?>">
-    <div class="homepage-calendar-feed-title"><?php print $title; ?>
-    <?php print $city; ?>, <?php print $state; ?></div>
+    <div class="homepage-calendar-feed-title-loc">
+      <div class="event-title"><?php print $title; ?></div>
+      <div class="event-loc"><?php print $city; ?>, <?php print $state; ?></div>
+    </div>
     <div class="homepage-calendar-feed-date"><?php print $date; ?></div>
-
   </a>
 </div>
